a0ef738198dc04c7d3f9335dbc35ca49ad75890e
Maksym Bykovskyy <maksym@bykovskyy.com>
2019-09-20 11:42:47 -0700

    Show balance sample app

--- README.md
+++ README.md
@@ -1,1 +1,25 @@
-# taquito-boilerplate-app
+# taquito-boilerplate
+
+A minimal framework-agnostic setup for starting developing Tezos DApps quickly with Taquito.
+
+## Dependencies
+1. jQuery - for simple manipulation and traversal of HTML documents.
+2. parcel-bundler - for packaging web applications quickly.
+
+## Getting Started
+
+1. Create a new repository from taquito-boilerplate by clicking "Use this template".
+2. Clone your new repository:
+
+    `git clone <YOUR_REPOSITORY_URL>`
+
+3. Change your current working directory to the newly cloned repository directory.
+4. Install dependencies:
+
+    `npm install`
+
+5. Start development server:
+
+    `npm run watch`
+
+6. Open http://localhost:1234 in your browser to see a sample application.
---
+++ src/app.ts
@@ -0,0 +1,43 @@
+import { Tezos, TezosToolkit } from '@taquito/taquito';
+import $ from 'jquery';
+
+export class App {
+  private tk: TezosToolkit = Tezos;
+
+  constructor() {
+    this.tk.setProvider({ rpc: 'https://rpc.tezrpc.me' });
+  }
+
+  public initUI() {
+    $('#show-balance-button').bind('click', () =>
+      this.getBalance($('#address-input').val())
+    );
+  }
+
+  private showError(message: string) {
+    $('#balance-output')
+      .removeClass()
+      .addClass('hide');
+    $('#error-message')
+      .removeClass()
+      .addClass('show')
+      .html('Error: ' + message);
+  }
+
+  private showBalance(balance: number) {
+    $('#error-message')
+      .removeClass()
+      .addClass('hide');
+    $('#balance-output')
+      .removeClass()
+      .addClass('show');
+    $('#balance').html(balance);
+  }
+
+  private getBalance(address: string) {
+    this.tk.rpc
+      .getBalance(address)
+      .then(balance => this.showBalance(balance.toNumber() / 1000000))
+      .catch(e => this.showError('Address not found'));
+  }
+}
